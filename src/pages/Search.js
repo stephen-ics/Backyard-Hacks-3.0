268e3d889c9287e64b0310215937a20e28ce025d
Stephen Ni <stephenni1234@gmail.com>
2022-07-30 16:53:33 -0400

    r
    Co-authored-by: Sean Wang <seanwaterloo2997@gmail.com>
    Co-authored-by: Izaan Qaiser <IzaanQ@users.noreply.github.com>
    Co-authored-by: bakuyy <bakuyy@users.noreply.github.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import PlantSection from '../components/PlantSection'
-import Grass from '../images/grass.png'
 import { useState, useEffect } from 'react'
 
 const Search = () => {
   const [plantData, setPlantData] = useState(null);
 
   useEffect(() => {
-    fetch('https://localhost:8000/PlantSection')
+    fetch('http://localhost:8000/PlantSection')
         .then(res => {
           return res.json();
         })
@@ -16,15 +15,13 @@ const Search = () => {
         });
     }, []);
 
-    console.log(plantData)
-
   return (
     <div className='flex w-full flex-col items-center'>
       <form className='w-3/4'>
         <input type='text' className='bg-white w-full pl-8 py-5 rounded-2xl' placeholder='Search for a Plant'></input>
       </form>
-      <div className='flex flex-wrap'>
-        {plantData && <div className='text-8x bg-black'>hi</div>}
+      <div className='flex w-full flex-row w-3/4'>
+        {plantData && <PlantSection plants={plantData} />}
       </div>
     </div>
   )
