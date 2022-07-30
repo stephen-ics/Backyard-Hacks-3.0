ce6bd36086192dbe75c2f78992ab8f943323e210
Stephen Ni <stephenni1234@gmail.com>
2022-07-30 03:31:25 -0400

    Co-authored-by: bakuyy <bakuyy@users.noreply.github.com> Co-authored-by: Izaan Qaiser <IzaanQ@users.noreply.github.com>

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const Dashboard = () => {
+  return (
+    <div>
+        {DataTransfer.map((Plants) => {
+        }
+        )}
+    </div>
+  )
+}
+
+const Plants = () => {
+  return (
+    {
+      id: 1,
+      img: 'https://www.starrosesandplants.com/plants/elle/',
+      name: 'Rose',
+    },
+    {
+      id: 2,
+      img: 'https://www.torontoflowergallery.com/products/eucalyptus',
+      name: 'Eculyptus',
+    },
+  )
+}
+export default Dashboard
