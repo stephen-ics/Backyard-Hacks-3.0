2b60d63ced2ea8179ca8a20b97bd16ffbc1c103a
Stephen Ni <stephenni1234@gmail.com>
2022-07-30 04:31:20 -0400

    Search bar  + dashboard improvements
    Co-authored-by: bakuyy <bakuyy@users.noreply.github.com>

--- src/components/PlantSection.js
+++ src/components/PlantSection.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const PlantSection = ({ title, image, id }) => {
-  return (
-    <motion.div className='bg-green-600 rounded-xl w-1/2 py-4 pl-8 ml-20 flex items-center mt-5 mb-5 shadow-2xl'
+    return (
+    <motion.div className='bg-green-600 rounded-xl w-2/5 py-4 pl-8 ml-20 flex items-center mt-5 mb-5 shadow-2xl'
     whileHover={{scale:1.1}}
     whileTap={{scale:0.9}}>
-        <a href={`/search/${id}`} className='flex items-center'>
+        <a href={`/browse/${id}`} className='flex items-center'>
             <img src={image} className='w-20 h-20 rounded-full'/>
             <h1 className='ml-5 text-white text-4xl'>{title}</h1>
         </a>
